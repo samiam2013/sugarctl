defc340072d038ae0f2f6b680cb53775e1431d45
samiam2013 <samiam2013@gmail.com>
2026-05-10 18:02:49 -0500

    make it loop, refactor to reciever methods

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -42,41 +41,53 @@ func main() {
 		"user_len", len(userInput),
 		"pass_len", len(passInput))
 
-	acc, err := login(userInput, passInput)
+	acc := NewAccount(userInput, passInput)
+	err := acc.login()
 	if err != nil {
 		slog.Error("Login failed", "error", err)
 		return
 	}
 	slog.Info("Login successful", "account", acc)
 
-	slog.Info("waiting 10 seconds for the api servers to sync")
-	time.Sleep(10 * time.Second)
+	for {
+		connData, err := acc.getConnections()
+		if err != nil {
+			slog.Error("Failed getting connections", "error", err)
+			return
+		}
 
-	cons, err := getConnections(acc)
-	if err != nil {
-		slog.Error("Failed getting connections", "error", err)
-		return
+		fmt.Printf("sugar level: %d mg/dL\n",
+			connData.Data[0].GlucoseMeasurement.ValueInMgPerDl)
+		time.Sleep(time.Second * 60)
 	}
-	_ = cons
 
 }
 
 type account struct {
+	user      string
+	pass      string
 	accountID string
 	token     string
 }
 
+func NewAccount(user, pass string) account {
+	acc := account{
+		user: user,
+		pass: pass,
+	}
+	return acc
+}
+
 // login takes the user and pass and returns the token and account id
-func login(user, pass string) (account, error) {
-	acc := account{}
+func (acc *account) login() error {
 	u, err := url.Parse(baseURL + "/llu/auth/login")
 	if err != nil {
-		return acc, fmt.Errorf("Failed to parse login URL: %w", err)
+		return fmt.Errorf("Failed to parse login URL: %w", err)
 	}
-	bodyMap := map[string]string{"email": user, "password": pass}
+	bodyMap := map[string]string{"email": acc.user, "password": acc.pass}
 	b, err := json.Marshal(bodyMap)
 	if err != nil {
-		return acc, fmt.Errorf("Failed to marshal body: %w", err)
+		return fmt.Errorf("Failed to marshal body: %w", err)
 	}
 	r, err := http.NewRequest(
 		http.MethodPost,
@@ -93,36 +104,34 @@ func login(user, pass string) (account, error) {
 	client := &http.Client{}
 	resp, err := client.Do(r)
 	if err != nil {
-		return acc, fmt.Errorf("Failed to make request: %w", err)
+		return fmt.Errorf("Failed to make request: %w", err)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return acc, fmt.Errorf("Failed to read response body: %w", err)
+		return fmt.Errorf("Failed to read response body: %w", err)
 	}
 
 	lr := loginResponse{}
 	if err := json.Unmarshal(body, &lr); err != nil {
-		return acc, fmt.Errorf("Failed to unmarshal response: %w", err)
+		return fmt.Errorf("Failed to unmarshal response: %w", err)
 	}
 
 	acc.token = lr.Data.AuthTicket.Token
 	acc.accountID = lr.Data.User.ID
-	return acc, nil
+	return nil
 }
 
-type connections struct{}
-
-func getConnections(acc account) (connections, error) {
-	cons := connections{}
+func (acc *account) getConnections() (connectionsResponse, error) {
+	connData := connectionsResponse{}
 	u, err := url.Parse(baseURL + "/llu/connections")
 	if err != nil {
-		return cons, fmt.Errorf("Failed to parse URL for request: %w", err)
+		return connData, fmt.Errorf("Failed to parse URL for request: %w", err)
 	}
 
 	r, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
-		return cons, fmt.Errorf("Failed to create request for request: %w", err)
+		return connData, fmt.Errorf("Failed to create request for request: %w", err)
 	}
 	sha256id := sha256.New()
 	sha256id.Write([]byte(acc.accountID))
@@ -138,14 +147,17 @@ func getConnections(acc account) (connections, error) {
 	c := http.Client{}
 	resp, err := c.Do(r)
 	if err != nil {
-		return cons, fmt.Errorf("Failed to do request for request: %w", err)
+		return connData, fmt.Errorf("Failed to do request for request: %w", err)
 	}
 	defer resp.Body.Close()
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return cons, fmt.Errorf("Failed to read response body for request: %w", err)
+		return connData, fmt.Errorf("Failed to read response body for request: %w", err)
 	}
-	fmt.Println(string(b))
 
-	return cons, errors.New("Implementation not finished")
+	if err := json.Unmarshal(b, &connData); err != nil {
+		return connData, fmt.Errorf("Failed to unmarshal response data for request: %w", err)
+	}
+
+	return connData, nil
 }
